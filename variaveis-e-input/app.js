f9e3c88eedf2be9a850d90e24231b6f0037567d8
guaxelo <silvaugusto222@gmail.com>
2022-06-23 18:30:01 -0300

    variaveis-e-input

--- variaveis-e-input/app.js
+++ variaveis-e-input/app.js
@@ -5,7 +5,7 @@ const readline = require('readline').createInterface({
 
 let leituraDeCampo;
 let teste = 8
-readline.question('infome sua idade: ', t => {
-    leituraDeCampo = t;
+readline.question('infome sua idade: ', input => {
+    leituraDeCampo = input;
     console.log(`O Usuario digitou: ${leituraDeCampo} e nossa variavel teste é: ${!teste}`);
 });
