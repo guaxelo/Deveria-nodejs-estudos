e134c986e9262e80dfb5c9152599693ef305d1ba
Carlos Silva <88803987+guaxelo@users.noreply.github.com>
2022-06-25 06:20:33 -0300

    Delete hello-world-nextjs directory

--- hello-world-nextjs/pages/api/data.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import moment from 'moment'
-
-
-export default (req : NextApiRequest, res : NextApiResponse) =>{
-    res.status(200).json({data : moment().format('DD/MM/YYYY HH:mm:ss')})
-}
